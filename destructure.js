048158c5b8b4a62d372b214af19afe2f32fc494e
nafiul-shishir <nafiul.shishir@gmail.com>
2020-11-27 12:14:00 +0600

    module finished

---
+++ destructure.js
@@ -0,0 +1,5 @@
+const person = {name:'Jack William', age: 17, job: 'youtuber', gfName: 'Ema Watson', address:' Nil Khet', phone: '01687110333', friends: ['sadiq','bishal','piyash','rocky']};
+
+const { phone, name, address } = person
+
+console.log(phone,name,address);
